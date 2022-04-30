9337c1b040bcbc0ca95742b757f61f473dd68e0e
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-04-30 23:39:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import StopWatch from './components/StopWatch';
-import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
-      <Timer></Timer>
-      {/* <StopWatch></StopWatch> */}
+      <StopWatch></StopWatch>
     </div>
   );
 }
